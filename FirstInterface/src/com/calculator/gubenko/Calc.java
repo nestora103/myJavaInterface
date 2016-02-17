43d2787af12b72f04ac741871815769cfdef8798
DmitriX <nestora103@gmail.com>
2016-02-17 16:59:30 +0300

    Добавление всех файлов проекта в репо

---
+++ FirstInterface/src/com/calculator/gubenko/Calc.java
@@ -0,0 +1,10 @@
+package com.calculator.gubenko;
+
+/**
+ * Created by DmitriX on 17.02.2016.
+ */
+public interface Calc {
+    int plus(int fVal,int tVal);
+    int minus(int fVal,int tVal);
+    float dev(int fVal,int tVal);
+}
